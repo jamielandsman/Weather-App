23a5614ec4263b770fd907e25e2847a5b7486d62
Jamie Landsman <jamie@userscape.com>
2023-06-04 09:47:50 -0400

    still

--- src/scripts.js
+++ src/scripts.js
@@ -132,13 +132,13 @@ function search() {
   let searchForm = document.getElementById("search-form");
   let locationInput = document.getElementById("searchInput");
 
-  searchForm.addEventListener("submit", async function (event) {
+  searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
     let location = locationInput.value.trim();
     let [city, country] = location.split(",");
 
-    await geocodingCityCountry(city, country);
+    geocodingCityCountry(city, country);
     reverseGeocode();
   });
 
